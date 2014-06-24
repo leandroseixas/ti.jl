a3ba6ea377a9551caf695353b599747f7df1aa83
Leandro Seixas <seixasle@gmail.com>
2014-06-24 15:36:10 -0300

    create plot.jl to plot graphs with Gadfly

---
+++ plot.jl
@@ -0,0 +1,21 @@
+#!/usr/bin/env julia
+
+##
+# Plot graph with Gadfly (Julia)
+#
+
+using DataFrames
+using Gadfly
+using Color
+
+bands = readtable("bands.csv")
+
+function multiplot(df, x, ys, args...)
+  plot( df, tuple( [ layer(x=x, y=yi, args...) for yi=ys ]... )... )
+end
+
+mp = multiplot(bands, 1, 2:5,
+Theme(default_color=color("#0033ff"), panel_fill=color("#efefef"), grid_color=color("#fefefe")),
+Geom.line)
+
+draw(PDF("bands.pdf", 10cm, 10cm), mp)
